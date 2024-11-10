cfcf69d26d441199dbc33bcdaaf5092573f8ef4e
Gopal-nd <gopalhelpyou@gmail.com>
2024-11-10 22:30:36 +0530

    added icons

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,28 +1,66 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		 require("neo-tree").setup({
-                -- Configure NeoTree with default settings
-                close_if_last_window = true,
-                filesystem = {
-                    filtered_items = {
-                        hide_dotfiles = false,
-                        hide_gitignored = true,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",             -- A Lua library for Neovim that provides useful utilities.
+        "nvim-tree/nvim-web-devicons",      -- Adds support for file icons.
+        "MunifTanjim/nui.nvim",             -- Required for NeoTree's UI elements.
+    },
+    config = function()
+        -- Setup NeoTree with icons
+        require("neo-tree").setup({
+            -- Close NeoTree when it’s the last window
+            close_if_last_window = true,
+
+            -- Filesystem Configuration
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,       -- Show dotfiles in the file explorer
+                    hide_gitignored = true,      -- Hide files ignored by git
+                },
+                use_libuv_file_watcher = true,  -- Use libuv to watch for file changes
+            },
+
+            -- Window Configuration
+            window = {
+                position = "left",    -- Position of NeoTree: left side of the screen
+                width = 30,           -- Width of the NeoTree window
+                mappings = {
+                    ["<space>"] = "toggle_node",  -- Spacebar to toggle a node
+                    ["<cr>"] = "open",           -- Enter to open the selected file
+                },
+            },
+
+            -- Enabling icons
+            renderer = {
+                icons = {
+                    glyphs = {
+                        default = "",          -- Default file icon
+                        symlink = "",          -- Symlink icon
+                        git = {
+                            unstaged = "",     -- Unstaged file icon
+                            staged = "S",        -- Staged file icon
+                            untracked = "U",     -- Untracked file icon
+                            deleted = "",       -- Deleted file icon
+                            ignored = "◌",       -- Ignored file icon
+                        },
+                        folder = {
+                            default = "",       -- Default folder icon
+                            open = "",          -- Open folder icon
+                            empty = "",         -- Empty folder icon
+                            empty_open = "",    -- Empty open folder icon
+                        },
                     },
                 },
-                window = {
-                    position = "left",
-                    width = 30,
-                },
-            })
-               vim.api.nvim_set_keymap('n', '<leader>n', ':Neotree toggle<CR>', { noremap = true, silent = true })
- 
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
+            },
+        })
+
+        -- Key mappings for toggling NeoTree and opening buffer float
+        -- Toggle NeoTree file explorer with <leader>n
+        vim.api.nvim_set_keymap('n', '<leader>n', ':Neotree toggle<CR>', { noremap = true, silent = true })
+
+        -- Reveal buffers in a floating window with <leader>bf
+        vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    end,
 }
+
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,9 +5,9 @@ return {
     config = function()
 	local config = require("nvim-treesitter.configs")
 config.setup({
-ensure_installed = { "c", "javascript","lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+ensure_installed = { "c","cpp", "javascript","lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 auto_install = true,
-highlight = {enable=true},
+highlight = {enable=true,disable = {"cpp"}},
 indent = {enable=true},
 })
      end
